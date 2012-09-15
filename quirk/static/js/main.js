d6ba43901e989189d9ea633a402378a65291d6b3
Kevin Schaefer <kevinjschaefer@gmail.com>
2012-09-15 13:14:33 -0400

    JCrop

--- quirk/static/js/main.js
+++ quirk/static/js/main.js
@@ -14,10 +14,16 @@ $(document).ready(function() {
 
 	    // center column
 	    if (((images.length - 1) % 3) == 1) {
-	    	$('#main').append('<div class="screenContainer centerColumn"><img src="' + url + '"></div>');
+	    	$('#main').append('<div id="screen' + images.length + '" class="screenContainer centerColumn"><img src="' + url + '"></div>');
 	    } else {
-	    	$('#main').append('<div class="screenContainer"><img src="' + url + '"></div>');
+	    	$('#main').append('<div id="screen' + images.length + '" class="screenContainer"><img src="' + url + '"></div>');
 	    }
+
+	    $('#' + images.length).Jcrop({
+		    onChange: showCoords,
+		    onSelect: showCoords,
+		    onRelease: clearCoords
+		});
 
 	    $('#main').append('<div id="dragScreen" class="screenContainer"></div>');
 	    createDrop();
@@ -48,3 +54,51 @@ $(document).ready(function() {
 
 	createDrop();
 });
+
+object = document.getElementById("image");// get image object
+object.addEventListener ("mousedown", xy);// add mouse down listener to object
+
+// test mouse handler by showing x and y coordinates
+function xy(e) {
+	coords = relMouseCoords(e);
+	console.log('(' + coords.x + ', ' + coords.y + ')');
+}
+
+function relMouseCoords(e){
+	if (e.offsetX !== undefined && e.offsetY !== undefined) 
+		{ return {x:e.offsetX, y:e.offsetY}; }
+	
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = this;
+
+    do{
+        totalOffsetX += currentElement.offsetLeft;
+        totalOffsetY += currentElement.offsetTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = e.pageX - totalOffsetX;
+    canvasY = e.pageY - totalOffsetY;
+
+    return {x:canvasX, y:canvasY}
+}
+
+function showCoords(c) {
+	console.log('coords accepted');
+	/*
+	  $('#x1').val(c.x);
+	  $('#y1').val(c.y);
+	  $('#x2').val(c.x2);
+	  $('#y2').val(c.y2);
+	  $('#w').val(c.w);
+	  $('#h').val(c.h);
+	*/
+};
+
+function clearCoords() {
+	console.log('clear coords');
+  	//$('#coords input').val('');
+};
