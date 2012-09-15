c1e1293128735650665843cd5bdc161d521f8825
Kevin Schaefer <kevinjschaefer@gmail.com>
2012-09-15 11:22:11 -0400

    Static setup

--- quirk/settings.py
+++ quirk/settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/dotcloud/volatile/static/'
+STATIC_ROOT = '/home/dotcloud/volatile/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,6 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'static/')
 )
 
 # List of finder classes that know how to find static files in
