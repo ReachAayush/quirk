5be3c8c6a1b470feb5b85921d4018a24dca92f0e
Kevin Schaefer <kevinjschaefer@gmail.com>
2012-09-16 00:00:52 -0400

    mobile

--- quirk/static/js/mobile.js
+++ quirk/static/js/mobile.js
@@ -27,19 +27,19 @@ $(document).ready(function() {
 	    	https://www.filepicker.io/api/file/y0VknbUzTi6yLNaUWUlW/convert?w=240&h=100
 	    	$('#screens').append('<img style="width:100%;height:100%;" id="' + key + '" src="' + val[0] + '">');
 	    	var screen = new Object();
-	    	screen.x1 = val[1];
-	    	screen.y1 = val[2];
-	    	screen.x2 = val[3];
-	    	screen.y2 = val[4];
+	    	screen.x1 = scaleX(val[1]);
+	    	screen.y1 = scaleY(val[2]);
+	    	screen.x2 = scaleX(val[3]);
+	    	screen.y2 = scaleY(val[4]);
 	    	screens.push(screen);
 	    	hideAddressBar();
 	    } else {
 	    	$('#screens').append('<img style="width:100%;height:100%;" id="' + key + '" src="' + val[0] + '" class="hidden">');
 	    	var screen = new Object();
-	    	screen.x1 = val[1];
-	    	screen.y1 = val[2];
-	    	screen.x2 = val[3];
-	    	screen.y2 = val[4];
+	    	screen.x1 = scaleX(val[1]);
+	    	screen.y1 = scaleY(val[2]);
+	    	screen.x2 = scaleX(val[3]);
+	    	screen.y2 = scaleY(val[4]);
 	    	screens.push(screen);
 	    }
 	  });
@@ -49,3 +49,11 @@ $(document).ready(function() {
 	});
 	
 });
+
+function scaleX(num) {
+	return (num * $(window).width()) / 294;
+}
+
+function scaleY(num) {
+	return (num * $(window).height()) / 420;
+}
